dc76c215bf8e0a73836486c7a9e2f53ed5c6c5e4
mudasira42 <mudassirasultana42@gmail.com>
2025-07-15 01:16:48 +0530

    Update script.js

--- frontend/static/script.js
+++ frontend/static/script.js
@@ -1,4 +1,4 @@
-const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; // Update after deployment
+const contractAddress = ""; // Update after deployment
 
 const contractABI = [
   {
